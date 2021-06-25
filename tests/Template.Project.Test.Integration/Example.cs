b29e47232b5abfd3ab6e70777126c279cf04351f
Weslley Carneiro <weslley@outlook.com>
2021-06-25 13:09:00 -0300

    atualização SDK

---
+++ tests/Template.Project.Test.Integration/Example.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Template.Project.Test.Integration
+{
+    public class Example
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ tests/Template.Project.Test.Unit/Example.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Template.Project.Test.Unit
+{
+    public class Example
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
